b2bd74fa268a44c2a32e35523b1a1b90fa52a325
tylerbmitchell <tylerbmitchell6@gmail.com>
2018-05-04 18:48:51 -0500

    add python automation

---
+++ automate_analysis.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from os import popen
+from subprocess import call
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option('data', 'd', action='store_true',
+                  default=False, dest='isData',
+                  help='run on data or MC'
+                  )
+parser.add_option('exe', 'e', action='store',
+                  default='analyzer', dest='exe',
+                  help='name of executable'
+                  )
+(options, args) = parser.parse_args()
+
+if options.isData:
+    path = "/store/user/tmitchel/smhet_22feb_SV/"
+else:
+    path = "/store/user/tmitchel/smhet_20march/"
+
+fileList = [ifile for ifile in filter(None, popen('xrdfsls root://cmseos.fnal.gov/ ls '+path).read().split('\n'))]
+for ifile in fileList:
+    call('./'+options.exe+' '+ifile, shell=True)
