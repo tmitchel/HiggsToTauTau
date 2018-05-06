33f8165105639872d115aa8dee50661ec09bd4e6
Tyler Mitchell <tmitchel@fnal.gov>
2018-05-06 18:29:02 -0500

    another bug fix

--- analyze.cc
+++ analyze.cc
@@ -75,6 +75,7 @@ int main(int argc, char* argv[]) {
   int  binnum_pth = sizeof(bins_pth)/sizeof(Float_t) - 1;
   int  binnum_taupt = sizeof(bins_taupt)/sizeof(Float_t) - 1;
   int  binnum_mjj = sizeof(bins_mjj)/sizeof(Float_t) - 1;
+  fout->cd();
 
   TH1F *n70=new TH1F ("n70", "n70", 6,0,6);
 
@@ -180,6 +181,7 @@ int main(int argc, char* argv[]) {
       sf_id = id_SF->getSF(electron.getPt(), electron.getEta());
       evtwt *= (sf_trg * sf_id * h_Trk->Eval(electron.getEta()) * lumi_weights->weight(events.getNPU()));
     }
+    fout->cd();
 
     // Tau energy scale corrections
     // ...
