5453376704636fcf4521dde6c7f1837a21b6b56e
Tyler Mitchell <tyler106@ksu.edu>
2018-08-29 10:41:13 -0500

    update the QCD script

--- createQCD.py
+++ createQCD.py
@@ -3,13 +3,13 @@
 from ROOT import *
 
 fout = TFile('output/QCD_output.root', 'recreate')
-fdata = TFile('output/data.root', 'read')
+fdata = TFile('output/mela_svfit_full/Data.root', 'read')
 fout.cd()
 
 nom = 'grabbag/'
 qcd = 'et_antiiso_'
 
-bkg_files = ['ZL', 'ZTT', 'W_unscaled', 'VV', 'ttbar']
+bkg_files = ['ZL', 'ZTT', 'ZJ', 'EWKZ', 'W_unscaled', 'VV', 'TTT', 'TTJ']
 hists_1d = [
         ('tau_pt' , fdata.Get(nom+'tau_pt_SS') .Clone(), fdata.Get(nom+'tau_pt_QCD') .Clone()),
         ('el_pt'  , fdata.Get(nom+'el_pt_SS')  .Clone(), fdata.Get(nom+'el_pt_QCD')  .Clone()),
@@ -21,14 +21,14 @@ hists_1d = [
         ('mt'     , fdata.Get(nom+'mt_SS')     .Clone(), fdata.Get(nom+'mt_QCD')     .Clone()),
         ]
 hists_2d = [
-        ('0jet'   , fdata.Get(qcd+'0jet_crSS/Data')   .Clone(), fdata.Get(qcd+'0jet_cr/Data')   .Clone()),
-        ('boosted', fdata.Get(qcd+'boosted_crSS/Data').Clone(), fdata.Get(qcd+'boosted_cr/Data').Clone()),
-        ('ZH'     , fdata.Get(qcd+'ZH_crSS/Data')     .Clone(), fdata.Get(qcd+'ZH_cr/Data')     .Clone()),
-        ('vbf'    , fdata.Get(qcd+'vbf_crSS/Data')    .Clone(), fdata.Get(qcd+'vbf_cr/Data')    .Clone()),
+        ('0jet'   , fdata.Get(qcd+'0jet_crSS/data_obs')   .Clone(), fdata.Get(qcd+'0jet_cr/data_obs')   .Clone()),
+        ('boosted', fdata.Get(qcd+'boosted_crSS/data_obs').Clone(), fdata.Get(qcd+'boosted_cr/data_obs').Clone()),
+        ('ZH'     , fdata.Get(qcd+'ZH_crSS/data_obs')     .Clone(), fdata.Get(qcd+'ZH_cr/data_obs')     .Clone()),
+        ('vbf'    , fdata.Get(qcd+'vbf_crSS/data_obs')    .Clone(), fdata.Get(qcd+'vbf_cr/data_obs')    .Clone()),
         ]
 
 for ifile in bkg_files:
-    fbkg = TFile('output/'+ifile+'.root', 'READ')
+    fbkg = TFile('output/mela_svfit_full/'+ifile+'.root', 'READ')
     fout.cd()
     for ihist in hists_1d:
         hname = nom+ihist[0]
@@ -48,16 +48,20 @@ for ifile in bkg_files:
 
 fout.mkdir('grabbag')
 fout.cd('grabbag')
-for ihist in hists_1d:
-  ihist[2].Scale(ihist[1].Integral()/ihist[2].Integral())
-  ihist[2].SetName(ihist[0])
-  ihist[2].Write()
+#for ihist in hists_1d:
+#  ihist[2].Scale(ihist[1].Integral()/ihist[2].Integral())
+#  ihist[2].SetName(ihist[0])
+#  ihist[2].Write()
 
 for ihist in hists_2d:
+    print ihist[0]
     idir = fout.mkdir('et_'+ihist[0])
     idir.cd()
-    ihist[2].Scale(ihist[1].Integral()/ihist[2].Integral())
-    ihist[2].SetName('qcd')
+    if ihist[2].Integral() > 0:
+      ihist[2].Scale(ihist[1].Integral()/ihist[2].Integral())
+    else:
+      ihist[2].Scale(0)
+    ihist[2].SetName('QCD')
     ihist[2].Write()
 
 fout.Close()
