553bddf002ac54b3cce7c95c85327c472955e076
Tyler Mitchell <tyler106@ksu.edu>
2018-09-26 14:10:36 -0500

    add some variables to the tree

--- include/slim_tree.h
+++ include/slim_tree.h
@@ -22,20 +22,55 @@ public:
     // member data
     TTree* otree;
     Int_t cat_0jet, cat_boosted, cat_vbf, cat_inclusive;
-    Float_t evtwt, higgs_pT, higgs_m, hjj_pT, hjj_m, dEtajj, dPhijj;
-    // TBranch *b_weight, *b_passSelection, *b_higgs_pT, *b_higgs_m, *b_hjj_pT, *b_hjj_m, *b_dEtajj, *b_dPhijj;
+    Float_t evtwt,
+        el_pt, el_eta, el_phi, el_mass,
+        mu_pt, mu_eta, mu_phi, mu_mass,
+        t1_pt, t1_eta, t1_phi, t1_mass, // t1 is used for et and mt, as well
+        t2_pt, t2_eta, t2_phi, t2_mass,
+        j1_pt, j1_eta, j1_phi,
+        j2_pt, j2_eta, j2_phi,
+        b1_pt, b1_eta, b1_phi,
+        b2_pt, b2_eta, b2_phi,
+        met, metphi,
+        higgs_pT, higgs_m, hjj_pT, hjj_m, dEtajj, dPhijj;
 };
 
-slim_tree::slim_tree(std::string tree_name) : otree( new TTree("tree_name", "tree_name") ) {
-//   b_weight        = otree->Branch("evtwt",         &weight,        "evtwt/F");
-//   b_passSelection = otree->Branch("passSelection", &passSelection, "passSelection/F");
-//   b_higgs_pT      = otree->Branch("higgs_pT",      &higgs_pT,      "higgs_pT/F");
-//   b_higgs_m       = otree->Branch("higgs_m",       &higgs_m,       "higgs_m/F");
-//   b_hjj_pT        = otree->Branch("hjj_pT",        &hjj_pT,        "hjj_pT/F");
-//   b_hjj_m         = otree->Branch("hjj_m",         &hjj_m,         "hjj_m/F");
-//   b_dEtajj        = otree->Branch("dEtajj",        &dEtajj,        "dEtajj/F");
-//   b_dPhijj        = otree->Branch("dPhijj",        &dPhijj,        "dPhijj/F");
+slim_tree::slim_tree(std::string tree_name) : otree( new TTree(tree_name.c_str(), tree_name.c_str()) ) {
     otree->Branch("evtwt",         &evtwt,         "evtwt/F"          );
+
+    otree->Branch("el_pt",         &el_pt,         "el_pt/F"          );
+    otree->Branch("el_eta",        &el_eta,        "el_eta/F"         );
+    otree->Branch("el_phi",        &el_phi,        "el_phi/F"         );
+    otree->Branch("el_mass",       &el_mass,       "el_mass/F"        );
+    otree->Branch("mu_pt",         &mu_pt,         "mu_pt/F"          );
+    otree->Branch("mu_eta",        &mu_eta,        "mu_eta/F"         );
+    otree->Branch("mu_phi",        &mu_phi,        "mu_phi/F"         );
+    otree->Branch("mu_mass",       &mu_mass,       "mu_mass/F"        );
+    otree->Branch("t1_pt",         &t1_pt,         "t1_pt/F"          );
+    otree->Branch("t1_eta",        &t1_eta,        "t1_eta/F"         );
+    otree->Branch("t1_phi",        &t1_phi,        "t1_phi/F"         );
+    otree->Branch("t1_mass",       &t1_mass,       "t1_mass/F"        );
+    otree->Branch("t2_pt",         &t2_pt,         "t2_pt/F"          );
+    otree->Branch("t2_eta",        &t2_eta,        "t2_eta/F"         );
+    otree->Branch("t2_phi",        &t2_phi,        "t2_phi/F"         );
+    otree->Branch("t2_mass",       &t2_mass,       "t2_mass/F"        );
+
+    otree->Branch("j1_pt",         &j1_pt,         "j1_pt/F"          );
+    otree->Branch("j1_eta",        &j1_eta,        "j1_eta/F"         );
+    otree->Branch("j1_phi",        &j1_phi,        "j1_phi/F"         );
+    otree->Branch("j2_pt",         &j2_pt,         "j2_pt/F"          );
+    otree->Branch("j2_eta",        &j2_eta,        "j2_eta/F"         );
+    otree->Branch("j2_phi",        &j2_phi,        "j2_phi/F"         );
+    otree->Branch("b1_pt",         &b1_pt,         "b1_pt/F"          );
+    otree->Branch("b1_eta",        &b1_eta,        "b1_eta/F"         );
+    otree->Branch("b1_phi",        &b1_phi,        "b1_phi/F"         );
+    otree->Branch("b2_pt",         &b2_pt,         "b2_pt/F"          );
+    otree->Branch("b2_eta",        &b2_eta,        "b2_eta/F"         );
+    otree->Branch("b2_phi",        &b2_phi,        "b2_phi/F"         );
+
+    otree->Branch("met",           &met,           "met/F"            );
+    otree->Branch("metphi",        &metphi,        "metphi/F"         );
+
     otree->Branch("higgs_pT",      &higgs_pT,      "higgs_pT/F"       );
     otree->Branch("higgs_m",       &higgs_m,       "higgs_m/F"        );
     otree->Branch("hjj_pT",        &hjj_pT,        "hjj_pT/F"         );
@@ -48,25 +83,49 @@ slim_tree::slim_tree(std::string tree_name) : otree( new TTree("tree_name", "tre
     otree->Branch("cat_inclusive", &cat_inclusive, "cat_inclusive/I"  );
 }
 
-void slim_tree::fillTree(std::string cat, electron* el, tau* t, jet_factory* fjets, met_factory* met, event_info* evt, Float_t weight) {
+void slim_tree::fillTree(std::string cat, electron* el, tau* t, jet_factory* fjets, met_factory* fmet, event_info* evt, Float_t weight) {
     // create things needed for later
     auto jets( fjets->getJets() );
-    TLorentzVector higgs( el->getP4() + t->getP4() + met->getP4() );
+    TLorentzVector higgs( el->getP4() + t->getP4() + fmet->getP4() );
 
     // start filling branches
     evtwt = weight;
     higgs_pT = higgs.Pt();
     higgs_m = higgs.M();
-    if (fjets->getNjets() > 1) {
-        hjj_pT = (higgs + jets.at(0).getP4() + jets.at(1).getP4()).Pt();
-        hjj_m = (higgs + jets.at(0).getP4() + jets.at(1).getP4()).M();
-        dEtajj = fabs(jets.at(0).getEta() - jets.at(1).getEta());
-        dPhijj = fabs(jets.at(0).getPhi() - jets.at(1).getPhi());
-    } else {
-        hjj_pT = 0.;
-        hjj_m = 0.;
-        dEtajj = 0.;
-        dPhijj = 0.;
+    el_pt = el->getPt();
+    el_eta = el->getEta();
+    el_phi = el->getPhi();
+    el_mass = el->getMass();
+    t1_pt = t->getPt();
+    t1_eta = t->getEta();
+    t1_phi = t->getPhi();
+    t1_mass = t->getMass();
+    met = fmet->getMet();
+    metphi = fmet->getMetPhi();
+
+    // dijet info is only ok if you have 2 jets, imagine that
+    hjj_pT = 0.;
+    hjj_m = 0.;
+    dEtajj = 0.;
+    dPhijj = 0.;
+    j1_pt = 0; j1_eta = 0; j1_phi = 0;
+    j2_pt = 0; j2_eta = 0; j2_phi = 0;
+    b1_pt = 0; b1_eta = 0; b1_phi = 0;
+    b2_pt = 0; b2_eta = 0; b2_phi = 0;
+
+    if (fjets->getNjets() > 0) {
+        j1_pt = jets.at(0).getPt();
+        j1_eta = jets.at(0).getEta();
+        j1_phi = jets.at(0).getPhi();
+        if (fjets->getNjets() > 1) {
+          j2_pt = jets.at(1).getPt();
+          j2_eta = jets.at(1).getEta();
+          j2_phi = jets.at(1).getPhi();
+          hjj_pT = (higgs + jets.at(0).getP4() + jets.at(1).getP4()).Pt();
+          hjj_m = (higgs + jets.at(0).getP4() + jets.at(1).getP4()).M();
+          dEtajj = fabs(jets.at(0).getEta() - jets.at(1).getEta());
+          dPhijj = fabs(jets.at(0).getPhi() - jets.at(1).getPhi());
+        }
     }
 
     // reset the categories
