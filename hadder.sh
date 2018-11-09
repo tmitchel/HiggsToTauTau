33c697b8a53f216bd0df752356e01b8566921174
Tyler Mitchell <tyler106@ksu.edu>
2018-11-09 13:15:24 -0600

    hadding 2017 files

--- hadder.sh
+++ hadder.sh
@@ -1,19 +1,28 @@
-hadd Output/trees//Data.root Output/trees//data*
-mv Output/trees//TT_TTT_output.root Output/trees//TTT.root
-mv Output/trees//TT_TTJ_output.root Output/trees//TTJ.root
-hadd Output/trees//ZJ.root Output/trees//DY*ZJ*
-hadd Output/trees//ZL.root Output/trees//DY*ZL*
-hadd Output/trees//mc_ZTT.root Output/trees//DY*ZTT*
-hadd Output/trees//ZTT.root Output/trees//embed-*
-hadd Output/trees//W.root Output/trees//WJet*.root
-hadd Output/trees//VV.root Output/trees//T-* Output/trees//Tbar* Output/trees//VV* Output/trees//WZ* Output/trees//ZZ* Output/trees//WW*
-hadd Output/trees//VVT.root Output/trees//T-*VVT* Output/trees//Tbar*VVT* Output/trees//VV*VVT* Output/trees//WZ*VVT* Output/trees//ZZ*VVT* Output/trees//WW*VVT*
-hadd Output/trees//VVJ.root Output/trees//T-*VVJ* Output/trees//Tbar*VVJ* Output/trees//VV*VVJ* Output/trees//WZ*VVJ* Output/trees//ZZ*VVJ* Output/trees//WW*VVJ*
+hadd Output/trees/Data.root Output/trees/data*
+hadd Output/trees/TTT.root Output/trees/*_TTT_*.root
+hadd Output/trees/TTJ.root Output/trees/*_TTJ_*.root
 
-hadd Output/trees//EWKZ.root Output/trees//EWKZ2*
-hadd Output/trees//WH125.root Output/trees//WPlus* Output/trees//WMinus*
-mv Output/trees//VBFHtoTauTau125_VBF125_output.root Output/trees/VBF125.root
-mv Output/trees//ZHTauTau125_ZH125_output.root Output/trees/ZH125.root
-hadd Output/trees//ggH125.root Output/trees//ggHtoTauTau125*
-mkdir Output/trees//originals
-mv Output/trees//*output*.root Output/trees//originals
+hadd Output/trees/ZJ.root Output/trees/*_ZJ_*.root
+hadd Output/trees/ZL.root Output/trees/*_ZL_*.root
+hadd Output/trees/mc_ZTT.root Output/trees/DY*ZTT*
+hadd Output/trees/ZTT.root Output/trees/embed-*
+
+hadd Output/trees/W.root Output/trees/WJet*_W_*.root
+hadd Output/trees/W_v2.root Output/trees/*_W_*.root
+
+hadd Output/trees/VV.root Output/trees/*_VV_*.root
+hadd Output/trees/VVT.root Output/trees/*_VVT_*.root
+hadd Output/trees/VVJ.root Output/trees/*_VVJ_*.root
+
+hadd Output/trees/EWKZ.root Output/trees/*_EWKZ_*.root
+
+
+hadd Output/trees/WH125.root Output/trees/WPlus* Output/trees/WMinus*
+
+mv Output/trees/VBF125_output.root Output/trees/VBF125.root
+mv Output/trees/ZHTauTau125_ZH125_output.root Output/trees/ZH125.root
+hadd Output/trees/ggH125.root Output/trees/ggH125*
+mv Output/trees/ttH125_output.root Output/trees/ttH125.root
+
+mkdir Output/trees/originals
+mv Output/trees/*output*.root Output/trees/originals
