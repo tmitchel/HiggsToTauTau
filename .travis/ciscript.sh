db3020b27a74a4a23b98680640493bed824a34f4
Tyler Mitchell <tyler106@ksu.edu>
2018-11-15 08:49:31 -0600

    update other channels

--- .travis/ciscript.sh
+++ .travis/ciscript.sh
@@ -12,7 +12,8 @@ PROJECTDIR="$(dirname "${SELFDIR}")"
 sudo chmod 777 $PROJECTDIR
 cd $PROJECTDIR
 
-bash build plugins/et_analyzer.cc test_et
+bash build plugins/et_analyzer2016.cc test_et
+bash build plugins/et_analyzer2017.cc test_et
 bash build plugins/mt_analyzer.cc test_mt
 bash build plugins/tt_analyzer.cc test_tt
 bash build plugins/finalTemplate.cc test_final
