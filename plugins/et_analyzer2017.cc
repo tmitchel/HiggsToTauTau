cc76a55423de3ba10f324e5b6df3cb287840c7db
Tyler Mitchell <tyler106@ksu.edu>
2018-11-30 19:44:37 -0600

    add output dir

--- plugins/et_analyzer2017.cc
+++ plugins/et_analyzer2017.cc
@@ -46,6 +46,7 @@ int main(int argc, char* argv[]) {
   std::string sample = parser.Option("-s");
   std::string name = parser.Option("-n");
   std::string path = parser.Option("-p");
+  std::string output_dir = parser.Option("-d");
   std::string syst = parser.Option("-u");
   std::string fname = path + sample + ".root";
   bool isData = sample.find("data") != std::string::npos;
@@ -68,7 +69,7 @@ int main(int argc, char* argv[]) {
 
   // create output file
   auto suffix = "_output.root";
-  auto prefix = "Output/trees/";
+  auto prefix = "Output/trees/" + output_dir;
   std::string filename;
   if (name == sample) {
     filename = prefix + name + systname + suffix;
