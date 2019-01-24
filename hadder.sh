05f26eb102681d97ff236af3f2626809c7b01ff4
Tyler Mitchell <tyler106@ksu.edu>
2019-01-24 17:53:08 -0600

    add JHU to hadded

--- hadder.sh
+++ hadder.sh
@@ -8,7 +8,7 @@ hadd ${2}/TTJ.root ${2}/*_TTJ_*.root
 hadd ${2}/ZJ.root ${2}/*_ZJ_*.root
 hadd ${2}/ZL.root ${2}/*_ZL_*.root
 hadd ${2}/mc_ZTT.root ${2}/DY*ZTT*
-hadd ${2}/ZTT.root ${2}/embed${1}-*
+hadd ${2}/embedded.root ${2}/embed${1}-*
 
 hadd ${2}/W.root ${2}/WJets*_W_*.root
 
@@ -22,6 +22,11 @@ hadd ${2}/WH125.root ${2}/WPlus* ${2}/WMinus*
 hadd -f ${2}/VBF125.root ${2}/VBF*.root
 hadd -f ${2}/ZH125.root ${2}/ZH*.root
 hadd ${2}/ggH125.root ${2}/ggH*
+
+hadd ${2}/ggh_inc.root ${2}/ggh_*
+hadd ${2}/vbf_inc.root ${2}/vbf_*
+hadd ${2}/wh_inc.root ${2}/wh_*
+hadd ${2}/zh_inc.root ${2}/zh_*
 
 mkdir ${2}/originals
 mv ${2}/*output*.root ${2}/originals
