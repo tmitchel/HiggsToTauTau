db50a35a8957fda363de2467eac61bebe2ce7296
Tyler Mitchell <tyler106@ksu.edu>
2019-08-08 19:32:40 -0500

    starting to add categorization

--- scripts/produce_template.py
+++ scripts/produce_template.py
@@ -1,6 +1,10 @@
 import ROOT
+import time
+import numpy
+import pandas
 import uproot
 from glob import glob
+from array import array
 
 treename_conversion = {
     "_tree_UncMet_Up": "_CMS_scale_met_unclustered_13TeVUp",
@@ -40,9 +44,34 @@ categories = [
     "vbf_ggHMELA_bin11", "vbf_ggHMELA_bin12"
 ]
 
+decay_mode_bins = [0, 1, 10, 11]
+higgs_pT_bins_boost = [0, 100, 150, 200, 250, 300, 5000]
+mjj_bins = [300, 500, 10000]
+vis_mass_bins = [0, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 400]
+m_sv_bins_boost = [0, 80, 90, 100, 110, 120, 130, 140, 150, 160, 300]
+m_sv_bins_vbf = [0, 80, 100, 115, 130, 150, 1000]
+
+
+def build_histogram(name, x_bins, y_bins):
+    return ROOT.TH2F(name, name, len(x_bins) - 1, array('d', x_bins), len(y_bins) - 1, array('d', y_bins))
+
+
+def fill_fake_background(events, fake_file):
+    ff_file = ROOT.TFile(fake_file, 'READ')
+    ff = ff_file.Get('ff_comb')
+    zero_jet_events = events[events['njets'] == 0]
+    boosted_events = events[
+        (events['njets'] == 1) |
+        ((events['njets'] > 1) & events['mjj'] < 300)
+    ]
+    vbf_events = events[(events['njets'] > 1) & (events['mjj'] > 300)]
+
 
 def main(args):
-    channel_prefix = args.tree_name.replace('_tree', '')  # need prefix for things later
+    start = time.time()
+    # channel_prefix = args.tree_name.replace('_tree', '')  # need prefix for things later
+    channel_prefix = args.tree_name.replace('mutau_tree', 'mt')
+    channel_prefix = channel_prefix.replace('etau_tree', 'et')
     assert channel_prefix == 'mt' or channel_prefix == 'et', 'must provide a valid tree name'
     files = [ifile for ifile in glob('{}/*.root'.format(args.input_dir))]  # get files to process
 
@@ -51,23 +80,76 @@ def main(args):
     syst_name = '_Sys' if args.syst else '_noSys'
 
     output_file = ROOT.TFile('Output/templates/htt_{}_{}_{}_fa3_{}{}.root'.format(channel_prefix,
-                                                                                  ztt_name, syst_name, args.date, '_'+args.suffix))
-    
+                                                                                  ztt_name, syst_name, args.date, '_'+args.suffix), 'RECREATE')
+
     # create structure within output file
     for cat in categories:
         output_file.cd()
         output_file.mkdir('{}_{}'.format(channel_prefix, cat))
     output_file.cd()
 
+    # output_file = uproot.recreate('Output/templates/htt_{}_{}_{}_fa3_{}{}.root'.format(channel_prefix,
+    #                                                                               ztt_name, syst_name, args.date, '_'+args.suffix))
+
     for ifile in files:
-        name = ifile.replace('.root', '')
-        events = uproot.open(ifile)[args.tree_name]
-        print events.array('m_sv')[:10]
+        name=ifile.replace('.root', '')
+        print name
+        events=uproot.open(ifile)[args.tree_name].arrays([
+            'is_signal', 'is_antiTauIso', 'OS', 'nbjets', 'njets', 'mjj', 'evtwt', 'wt_*',
+            'mu_iso', 'el_iso', 't1_decayMode', 'vis_mass', 't1_pt', 'higgs_pT', 'm_sv',
+            'D0_VBF', 'D0_ggH', 'DCP_VBF', 'DCP_ggH', 'j1_phi', 'j2_phi', 'mt'
+        ], outputtype=pandas.DataFrame)
+
+        general_selection=events[
+            (events['mt'] < 50) & (events['nbjets'] == 0)
+        ]
+
+        # do signal categorization
+        signal_events=general_selection[general_selection['is_signal'] > 0]
+
+        zero_jet_events=signal_events[signal_events['njets'] == 0]
+        boosted_events=signal_events[
+            (signal_events['njets'] == 1) |
+            ((signal_events['njets'] > 1) & signal_events['mjj'] < 300)
+        ]
+        vbf_events=signal_events[(signal_events['njets'] > 1) & (signal_events['mjj'] > 300)]
+
+        # do anti-iso categorization for fake-factor for data
+
+        antiIso_events=general_selection[general_selection['is_antiTauIso'] > 0]
+        # fill_fake_background(antiIso_events, args.fake_file)
+
+        # uproot can't handle subdirectories yet, so I'll use plain old pyroot
+
+        # start with 0-jet category
+        output_file.cd('{}_0jet'.format(channel_prefix))
+        zero_jet_hist=build_histogram(name.split('/')[-1], decay_mode_bins, vis_mass_bins)
+        for i in xrange(len(zero_jet_events.index)):
+            zero_jet_hist.Fill(zero_jet_events['t1_decayMode'].values[i],
+                               zero_jet_events['vis_mass'].values[i], zero_jet_events['evtwt'].values[i])
+
+        # now boosted category
+        output_file.cd('{}_boosted'.format(channel_prefix))
+        boost_hist=build_histogram(name.split('/')[-1], higgs_pT_bins_boost, m_sv_bins_boost)
+        for i in xrange(len(boosted_events.index)):
+            boost_hist.Fill(boosted_events['higgs_pT'].values[i],
+                            boosted_events['m_sv'].values[i], boosted_events['evtwt'].values[i])
+
+        # vbf category is last
+        output_file.cd('{}_vbf'.format(channel_prefix))
+        vbf_hist=build_histogram(name.split('/')[-1], mjj_bins, m_sv_bins_vbf)
+        for i in xrange(len(vbf_events.index)):
+            vbf_hist.Fill(vbf_events['mjj'].values[i], vbf_events['m_sv'].values[i], vbf_events['evtwt'].values[i])
+
+        output_file.Write()
+
+    output_file.Close()
+    print 'Finished in {} seconds'.format(time.time() - start)
 
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
-    parser = ArgumentParser()
+    parser=ArgumentParser()
     parser.add_argument('--syst', '-s', action='store_true', help='run with systematics')
     parser.add_argument('--embed', '-e', action='store_true', help='use embedded instead of MC')
     parser.add_argument('--year', '-y', required=True, action='store', help='year being processed')
