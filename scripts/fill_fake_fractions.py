c1bed24f660a20d11020adb2dad6e800abaa908e
Tyler Mitchell <tyler106@ksu.edu>
2019-08-14 07:08:02 -0500

    initial skeleton

---
+++ scripts/fill_fake_fractions.py
@@ -0,0 +1,8 @@
+def main(args):
+    pass
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument()
+    main(parser.parse_args())
