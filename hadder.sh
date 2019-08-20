76f58a96b2561c95033ebb3ad5104decc574bdee
Tyler Mitchell <tyler106@ksu.edu>
2019-08-20 14:52:01 -0500

    change how files are hadded

--- hadder.sh
+++ hadder.sh
@@ -11,10 +11,10 @@ hadd ${2}/ZL.root ${2}/*_ZL_*.root
 hadd ${2}/ZTT.root ${2}/*_ZTT_*.root
 hadd ${2}/embedded.root ${2}/embed${1}-*
 
-hadd ${2}/EWKZ.root ${2}/EWKZ*.root
+# hadd ${2}/EWKZ.root ${2}/EWKZ*.root
 mv  ${2}/EWKZ2*.root ${2}/originals
 
-hadd ${2}/W_with_EWK.root ${2}/*_W_*.root
+# hadd ${2}/W_with_EWK.root ${2}/*_W_*.root
 hadd ${2}/W.root ${2}/WJ*_W_*.root
 
 hadd ${2}/VVT.root ${2}/*_VVT_*.root
@@ -25,15 +25,15 @@ hadd -f ${2}/vbf125_powheg.root ${2}/vbf125_powheg*.root
 hadd -f ${2}/zh125_powheg.root ${2}/zh125_powheg*.root
 hadd -f ${2}/ggh125_powheg.root ${2}/ggh125_powheg*
 
-hadd ${2}/ggh125_JHU.root ${2}/ggh125_JHU_*
-hadd ${2}/vbf125_JHU.root ${2}/vbf125_JHU_*
-hadd ${2}/wh125_JHU.root ${2}/wh125_JHU_*
-hadd ${2}/zh125_JHU.root ${2}/zh125_JHU_*
+hadd ${2}/ggh125_JHU.root ${2}/ggh125_JHU_*nom-decay*
+hadd ${2}/vbf125_JHU.root ${2}/vbf125_JHU_*nom-decay*
+hadd ${2}/wh125_JHU.root ${2}/wh125_JHU_*nom-decay*
+hadd ${2}/zh125_JHU.root ${2}/zh125_JHU_*nom-decay*
 
-hadd ${2}/ggh125_madgraph.root ${2}/ggh125_madgraph_*
+hadd ${2}/ggh125_madgraph.root ${2}/ggh125_madgraph_two_*nom-decay*
 hadd ${2}/vbf125_madgraph.root ${2}/vbf125_madgraph_*
 
 mv ${2}/*output*.root ${2}/originals
 
-cp ${2}/originals/*madgraph* ${2}
-cp ${2}/originals/*minlo* ${2}
+# cp ${2}/originals/*madgraph* ${2}
+# cp ${2}/originals/*minlo* ${2}
