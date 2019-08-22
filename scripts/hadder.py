ff092efc0cf6569bdf2afa5e185b5c3cba0bf1d7
Tyler Mitchell <tyler106@ksu.edu>
2019-08-22 13:40:02 -0500

    format

--- scripts/hadder.py
+++ scripts/hadder.py
@@ -12,37 +12,40 @@ def clean(hadd_list):
                 del hadd_list[idir][sample]
     return hadd_list
 
+
 def do_hadd(hadd_list, path):
     for idir, isamples in hadd_list.items():
         if not os.path.exists(path + '/' + idir + '/merged'):
             os.mkdir(path + '/' + idir + '/merged')
         for sample, files in isamples.items():
-            os.system('hadd {}/{}.root {}'.format(path + '/' + idir + '/merged', sample, ' '.join(files)))    
+            os.system('hadd {}/{}.root {}'.format(path + '/' + idir + '/merged', sample, ' '.join(files)))
+
 
 def combine_wh(hadd_list, path):
-  for idir in hadd_list.keys():
-    wh_files = []
-    for ifile in glob('{}/*.root'.format(path + '/' +idir)):
-      if 'wplus125' in ifile:
-        wh_files.append(ifile)
-      elif 'wminus125' in ifile:
-        wh_files.append(ifile)
-    if len(wh_files) == 2:
-      hadd_list[idir]['wh125_powheg'] = wh_files
-  return hadd_list
+    for idir in hadd_list.keys():
+        wh_files = []
+        for ifile in glob('{}/*.root'.format(path + '/' + idir)):
+            if 'wplus125' in ifile:
+                wh_files.append(ifile)
+            elif 'wminus125' in ifile:
+                wh_files.append(ifile)
+        if len(wh_files) == 2:
+            hadd_list[idir]['wh125_powheg'] = wh_files
+    return hadd_list
 
 
 def good_bkg(ifile):
-  if not 'EWK_W' in ifile and not 'EWKZ' in ifile:
-    return True
-  return False
+    if not 'EWK_W' in ifile and not 'EWKZ' in ifile:
+        return True
+    return False
+
 
 def good_sig(ifile):
-  if 'decay' in ifile and not 'nom-decay' in ifile:
-    return False
-  elif 'madgraph' in ifile and 'inc' in ifile:
-    return False
-  return True
+    if 'decay' in ifile and not 'nom-decay' in ifile:
+        return False
+    elif 'madgraph' in ifile and 'inc' in ifile:
+        return False
+    return True
 
 
 def main(args):
@@ -59,14 +62,14 @@ def main(args):
     bkg_hadd_list = {
         idir: {
             sample: [
-                ifile for ifile in glob('{}/*_{}_*.root'.format(args.path + '/' +idir, sample)) if good_bkg(ifile) 
+                ifile for ifile in glob('{}/*_{}_*.root'.format(args.path + '/' + idir, sample)) if good_bkg(ifile)
             ] for sample in bkgs
         } for idir in os.listdir(args.path) if os.path.isdir(args.path + '/' + idir) and not 'logs' in idir
     }
-    sig_hadd_list= {
+    sig_hadd_list = {
         idir: {
             sample: [
-                ifile for ifile in glob('{}/{}*.root'.format(args.path + '/' +idir, sample)) if good_sig(ifile)
+                ifile for ifile in glob('{}/{}*.root'.format(args.path + '/' + idir, sample)) if good_sig(ifile)
             ] for sample in signals
         } for idir in os.listdir(args.path) if os.path.isdir(args.path + '/' + idir) and not 'logs' in idir
     }
@@ -76,14 +79,13 @@ def main(args):
     sig_hadd_list = combine_wh(sig_hadd_list, args.path)
 
     with file('haddlog.txt', 'a') as outfile:
-      json.dump({
-        'background': bkg_hadd_list,
-        'signal': sig_hadd_list
-      }, outfile, sort_keys=True, indent=4, separators=(',', ': '))
+        json.dump({
+            'background': bkg_hadd_list,
+            'signal': sig_hadd_list
+        }, outfile, sort_keys=True, indent=4, separators=(',', ': '))
 
     do_hadd(bkg_hadd_list, args.path)
     do_hadd(sig_hadd_list, args.path)
-
 
 
 if __name__ == "__main__":
