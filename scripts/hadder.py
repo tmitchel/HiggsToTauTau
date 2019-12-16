4a8f3cd8db9b70db7719747b76debd0db2fcaf2f
Tyler Mitchell <tyler106@ksu.edu>
2019-12-16 09:14:22 -0600

    don't use vbf madgraph

--- scripts/hadder.py
+++ scripts/hadder.py
@@ -82,7 +82,7 @@ def main(args):
         'ggh125_JHU', 'vbf125_JHU', 'wh125_JHU', 'zh125_JHU',
 
         'ggh125_madgraph', 'ggh125_madgraph', 'ggh125_madgraph',
-        'vbf125_madgraph',
+        # 'vbf125_madgraph',
         'ggh125_powheg', 'vbf125_powheg', 'zh125_powheg'
     ]
     bkg_hadd_list = {
