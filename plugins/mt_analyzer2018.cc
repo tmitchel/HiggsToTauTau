21dc1d2ea07fe7b137c0b8dbc0c682d858acc473
Tyler Mitchell <tyler106@ksu.edu>
2020-01-14 13:47:07 -0600

    apply extra vetos in mt2018

--- plugins/mt_analyzer2018.cc
+++ plugins/mt_analyzer2018.cc
@@ -243,10 +243,16 @@ int main(int argc, char *argv[]) {
             histos->at("cutflow")->Fill(3., 1.);
         }
 
+        if (!event.hasDimuon() && !event.hasExtraElectron(lepton::MUON) && !event.hasExtraMuon(lepton::MUON)) {
+            histos->at("cutflow")->Fill(4., 1.);
+        } else {
+            continue;
+        }
+
         // anti-lepton discriminators
         if (tau.getAgainstTightMuonDeep() > 0.5 && tau.getAgainstVVVLooseElectronDeep() > 0.5
             && tau.getDecayModeFindingNew() && tau.getDecayMode() != 11) {
-            histos->at("cutflow")->Fill(4., 1.);
+            histos->at("cutflow")->Fill(5., 1.);
         } else {
             continue;
         }
@@ -254,7 +260,7 @@ int main(int argc, char *argv[]) {
         // only opposite-sign
         int evt_charge = tau.getCharge() + muon.getCharge();
         if (evt_charge == 0) {
-            histos->at("cutflow")->Fill(5., 1.);
+            histos->at("cutflow")->Fill(6., 1.);
         } else {
             continue;
         }
@@ -270,7 +276,7 @@ int main(int argc, char *argv[]) {
 
         // now do mt selection
         if (mt < 50) {
-            histos->at("cutflow")->Fill(6., 1.);
+            histos->at("cutflow")->Fill(7., 1.);
         } else {
             continue;
         }
@@ -430,7 +436,7 @@ int main(int argc, char *argv[]) {
 
         // b-jet veto
         if (jets.getNbtagLoose() < 2 && jets.getNbtagMedium() < 1) {
-            histos->at("cutflow")->Fill(7., 1.);
+            histos->at("cutflow")->Fill(8., 1.);
         } else {
             continue;
         }
@@ -441,7 +447,7 @@ int main(int argc, char *argv[]) {
 
         // only keep the regions we need
         if (signalRegion || antiTauIsoRegion) {
-            histos->at("cutflow")->Fill(8., 1.);
+            histos->at("cutflow")->Fill(9., 1.);
         } else {
             continue;
         }
