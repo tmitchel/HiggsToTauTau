1a83fb5a780ada7cb4d2774b924dec88aeac3664
Tyler Mitchell <tyler106@ksu.edu>
2020-01-31 09:56:48 -0600

    update cutflow

--- plugins/et_analyzer2018.cc
+++ plugins/et_analyzer2018.cc
@@ -243,7 +243,7 @@ int main(int argc, char* argv[]) {
         // only opposite-sign
         int evt_charge = tau.getCharge() + electron.getCharge();
         if (evt_charge == 0) {
-            histos->at("cutflow")->Fill(6., 1.);
+            histos->at("cutflow")->Fill(4., 1.);
         } else {
             continue;
         }
@@ -259,7 +259,7 @@ int main(int argc, char* argv[]) {
 
         // now do mt selection
         if (mt < 50) {
-            histos->at("cutflow")->Fill(7., 1.);
+            histos->at("cutflow")->Fill(5., 1.);
         } else {
             continue;
         }
@@ -443,7 +443,7 @@ int main(int argc, char* argv[]) {
 
         // b-jet veto
         if (jets.getNbtagLoose() < 2 && jets.getNbtagMedium() < 1) {
-            histos->at("cutflow")->Fill(8., 1.);
+            histos->at("cutflow")->Fill(6., 1.);
         } else {
             continue;
         }
@@ -454,7 +454,7 @@ int main(int argc, char* argv[]) {
 
         // only keep the regions we need
         if (signalRegion || antiTauIsoRegion) {
-            histos->at("cutflow")->Fill(9., 1.);
+            histos->at("cutflow")->Fill(7., 1.);
         } else {
             continue;
         }
