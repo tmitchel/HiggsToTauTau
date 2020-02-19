1fddf02581f5d36c6af2a7dacfd3db1b3819cc82
Tyler Mitchell <tyler106@ksu.edu>
2020-02-19 12:25:00 -0600

    add other directories

--- setup/setup-python.sh
+++ setup/setup-python.sh
@@ -1,5 +1,5 @@
 # can't use a normal requirements.txt because of weird /cvmfs stuff so here we are...
 
-python -m pip install -U numpy
-python -m pip install -U pandas
-python -m pip install -U uproot
+python -m pip install --isolated -U numpy
+python -m pip install --isolated -U pandas
+python -m pip install --isolated -U uproot
