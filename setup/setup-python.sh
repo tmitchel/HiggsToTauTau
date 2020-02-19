b67a68839a3370c74fae8e783e579c869627e46c
Tyler Mitchell <tyler106@ksu.edu>
2020-02-19 12:28:42 -0600

    add NN packages

--- setup/setup-python.sh
+++ setup/setup-python.sh
@@ -3,3 +3,6 @@
 python -m pip install --isolated -U numpy
 python -m pip install --isolated -U pandas
 python -m pip install --isolated -U uproot
+python -m pip install --isolated -U sklearn
+python -m pip install --isolated -U tensorflow
+python -m pip install --isolated -U keras
