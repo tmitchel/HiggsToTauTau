71a39a2ca5db2698cae5fe82e0ccf893b8433d90
jamesnatoli <james.v.natoli@gmail.com>
2020-06-12 08:19:35 -0500

    initialized deepiso wp vectors

--- include/models/tau.h
+++ include/models/tau.h
@@ -128,14 +128,17 @@ void tau::setDeepAgainstMuon(std::vector<Float_t> wps) {
 
 // initialize member data and set TLorentzVector
 tau::tau(Float_t _pt, Float_t _eta, Float_t _phi, Float_t _mass, Float_t _charge)
-    : pt(_pt),
-      eta(_eta),
-      phi(_phi),
-      mass(_mass),
-      charge(_charge),
-      mva_wps{0, 0, 0, 0, 0},
-      mva_againstel_wps{0, 0, 0, 0, 0},
-      mva_againstmu_wps{0, 0, 0, 0, 0} {
+: pt(_pt),
+  eta(_eta),
+  phi(_phi),
+  mass(_mass),
+  charge(_charge),
+  deep_wps{0, 0, 0, 0, 0, 0, 0, 0, 0},
+  deep_againstel_wps{0, 0, 0, 0, 0, 0, 0, 0, 0},
+  deep_againstmu_wps{0, 0, 0, 0, 0, 0, 0, 0, 0},
+  mva_wps{0, 0, 0, 0, 0},
+  mva_againstel_wps{0, 0, 0, 0, 0},
+  mva_againstmu_wps{0, 0, 0, 0, 0} {
     p4.SetPtEtaPhiM(pt, eta, phi, mass);
 }
 
